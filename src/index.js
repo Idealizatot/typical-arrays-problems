436b7b8568d348f40661525ebaf2009a1bb3d5f7
Idealizatot <ideal103.5@gmail.com>
2021-02-28 16:28:07 +0300

    feat: add body of functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array === 'undefined' || !array.length) {
+    return 0
+  }
+
+  let min = array.reduce((min, currentElem) => {
+    return currentElem < min ? currentElem : min;
+  }, Number.POSITIVE_INFINITY);
+
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array === 'undefined' || !array.length) {
+    return 0
+  }
+
+  let max = array.reduce((max, currentElem) => {
+    return currentElem > max ? currentElem : max;
+  }, Number.NEGATIVE_INFINITY);
+
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array === 'undefined' || !array.length) {
+    return 0
+  }
+
+  let avg =  array.reduce((sum, currentElem) => {
+    return sum + currentElem
+  }, 0) / array.length;
+
+  return avg;
+
+
+  return avg;
 }
